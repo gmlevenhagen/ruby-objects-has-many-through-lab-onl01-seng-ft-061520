d1960d55175f8212930dfd3b3cf552318d841bfc
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-23 12:54:33 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,23 +1,30 @@
 class Doctor
-  attr_accessor :name, :add_appointment
+  attr_accessor :name
+   @@all = []
 
-  def initialize(name)
-    @name = name
-    @appointments = []
-  end
+    def initialize(name)
+     @name = name
+     @@all << self
+   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.doctor = self
-  end
+    def appointments
+     Appointment.all.select do |appointment|
+       appointment.doctor == self
+     end
+   end
 
-  def appointments
-    @appointments
-  end
+    def self.all
+     @@all
+   end
 
-  def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
-  end
+    def new_appointment(patient, date)
+     Appointment.new(patient, date, self)
+   end
+
+   def patients
+       @appointments.collect do |appointment|
+         appointment.patient
+       end
+     end
+
  end
